a04fdfe089ff0223aa18a2c87b1e956ee54daa73
pedrojavier777 <pj.balarezo.hernandez@gmail.com>
2013-09-17 14:02:11 -0500

    Primer COMMIT

---
+++ SOAPServices/Persistencia/ConexionUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SOAPServices.Persistencia
+{
+    public class ConexionUtil
+    {
+        public static string ObtenerCadena()
+        {
+            return "Data Source=98942f8f-f8ff-4e5a-9e54-a23c0129cbc7.sqlserver.sequelizer.com;Initial Catalog=db98942f8ff8ff4e5a9e54a23c0129cbc7;User Id=wpqusibauiavzgdc;Password=RibMZZqC8qvjbqNVcMFMBsDKnUfzRhaqFDwzPyCatjWA728FNmWsQCKxLyyeUYTZ;";
+        }
+    }
+}
